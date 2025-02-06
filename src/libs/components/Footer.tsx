d44255ab57ff513db09777209cdf70d36c4bc45c
HK-SHAO <hk-shao@outlook.com>
2025-02-06 18:36:16 +0800

    Update Footer component styling and copyright information for clarity and responsiveness

--- src/libs/components/Footer.tsx
+++ src/libs/components/Footer.tsx
@@ -2,12 +2,15 @@ export default function Footer() {
   const year = new Date().getUTCFullYear();
   const toNow = year === 2025 ? '' : `-${year}`;
   return (
-    <div className="-mt-4 mb-10 p-0 w-full text-center">
+    <div className="mb-6 p-0 w-full text-center">
       <p className="m-0 p-0 font-bold text-base/snug text-gray-500">
-        © 2025{toNow} React Async for Client | built by{' '}
+        Copyright © 2025{toNow} React Async for Client
+        <br />
+        Built by{' '}
         <a href="https://github.com/HK-SHAO" target="_blank" rel="noreferrer">
           @HK-SHAO
-        </a>
+        </a>{' '}
+        MIT License
       </p>
     </div>
   );
