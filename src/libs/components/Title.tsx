79def1d3d7eeefab3cd0ba6e7a6f78db4cfde060
HK-SHAO <hk-shao@foxmail.com>
2025-02-06 20:26:34 +0800

    Refactor DemoPage and Title components for improved styling and responsiveness

--- src/libs/components/Title.tsx
+++ src/libs/components/Title.tsx
@@ -3,11 +3,11 @@ const title = 'React Async for Client';
 export default function Title() {
   return (
     <>
-      <div className="relative w-full min-h-[6rem] font-bold scale-120 hover:scale-150 transition-transform">
-        <div className="absolute flex justify-center items-center bg-clip-text blur-2xl py-4 w-full font-stretch-50% text-7xl text-center text-transparent pointer-events-none select-none gradient-bg">
+      <div className="relative w-full h-auto font-bold scale-100 hover:scale-130 transition-transform">
+        <div className="inset-0 flex justify-center items-center bg-clip-text blur-2xl py-4 w-full text-7xl text-center text-transparent pointer-events-none select-none gradient-bg">
           {title}
         </div>
-        <div className="absolute flex justify-center items-center bg-clip-text py-4 w-full font-stretch-50% text-7xl text-center text-transparent select-auto gradient-bg">
+        <div className="absolute inset-0 flex justify-center items-center bg-clip-text py-4 w-full text-7xl text-center text-transparent select-auto gradient-bg">
           {title}
         </div>
       </div>
