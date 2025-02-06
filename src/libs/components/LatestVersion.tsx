5e9916e502d3fa88e019465b99b03e28e694c073
HK-SHAO <hk-shao@outlook.com>
2025-02-06 15:01:51 +0800

    Add LatestVersion component to display current version and update DemoPage layout

---
+++ src/libs/components/LatestVersion.tsx
@@ -0,0 +1,26 @@
+import { useAsync } from '#src/lib';
+
+import delayWithSignal from '../utils/delayWithSignal';
+
+async function getLatestVersion(_: null, { signal }: { signal: AbortSignal }) {
+  const [version] = await Promise.all([
+    // ToDo: Dynamic import with signal
+    import('#constants/version').then((m) => m.default),
+    delayWithSignal(400, signal),
+  ]);
+  return version;
+}
+
+export default function LatestVersion() {
+  const {
+    state: { result: version = 'unknown', pending, error },
+  } = useAsync(getLatestVersion, null);
+
+  return (
+    <>
+      <div className="place-items-center grid bg-blue-600 dark:bg-blue-800 px-2 rounded-md font-bold font-stretch-120% text-white">
+        {pending ? 'Checking...' : error ? 'Error' : `v${version}`}
+      </div>
+    </>
+  );
+}
